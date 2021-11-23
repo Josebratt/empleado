d8bf20a7435ce71c8532a12197cf348b87ec8a96
Josebratt <josebratt@gmail.com>
2021-11-23 14:19:32 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: SignInComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: '**', pathMatch: 'full', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
