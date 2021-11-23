92057d791289cc69cda6d4f1ceab5f5de60e7096
Josebratt <josebratt@gmail.com>
2021-11-23 14:39:30 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'login-edd01',
+    appId: '1:928210507425:web:798cca04eaff277e4382e0',
+    databaseURL: 'https://login-edd01-default-rtdb.firebaseio.com',
+    storageBucket: 'login-edd01.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBv18eqfwO8I9b1YTiPBXWpT0JV409q5n4',
+    authDomain: 'login-edd01.firebaseapp.com',
+    messagingSenderId: '928210507425',
+  },
   production: true
 };
