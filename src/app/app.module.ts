d9a1c66fa7093ec49bbbb38faee754af35e1e242
Josebratt <josebratt@gmail.com>
2021-11-23 14:19:40 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SignInComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
