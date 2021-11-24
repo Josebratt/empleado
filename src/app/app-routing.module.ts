49ae1626638681130ed73909448ba23ee0378438
Josebratt <josebratt@gmail.com>
2021-11-23 19:31:13 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+import { ProfileComponent } from './auth/profile/profile.component';
+import { RegisterComponent } from './auth/register/register.component';
 import { SignInComponent } from './auth/sign-in/sign-in.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
 
 const routes: Routes = [
     { path: '', component: SignInComponent },
     { path: 'dashboard', component: DashboardComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'profile', component: ProfileComponent },
+    { path: 'sign-in', component: SignInComponent },
     { path: '**', pathMatch: 'full', redirectTo: '' },
 ];
 
